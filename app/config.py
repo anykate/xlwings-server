c2447f52b6555904756f8cff5d847c47b2fdf3ad
Aniket Aryamane <aryamane.aniket@gmail.com>
2024-09-29 19:51:13 +0530

    Updating config

--- app/config.py
+++ app/config.py
@@ -36,11 +36,11 @@ class Settings(BaseSettings):
     hostname: Optional[str] = None
     log_level: str = "INFO"
     # These UUIDs will be overwritten by: python run.py init
-    manifest_id_dev: UUID4 = "0a856eb1-91ab-4f38-b757-23fbe1f73130"
-    manifest_id_qa: UUID4 = "9cda34b1-af68-4dc6-b97c-e63ef6284671"
-    manifest_id_uat: UUID4 = "70428e53-8113-421c-8fe2-9b74fcb94ee5"
-    manifest_id_staging: UUID4 = "34041f4f-9cb4-4830-afb5-db44b2a70e0e"
-    manifest_id_prod: UUID4 = "4f342d85-3a49-41cb-90a5-37b1f2219040"
+    manifest_id_dev: UUID4 = "dcc4d25f-3fb0-4a65-84a7-a09178f1c89d"
+    manifest_id_qa: UUID4 = "52d6620d-f445-428b-b7d8-e4a6b40d4614"
+    manifest_id_uat: UUID4 = "1a421e95-576f-4ab2-9bfc-bb974d43a3e1"
+    manifest_id_staging: UUID4 = "96eb657a-b8b4-49a3-83a1-5e5357f6d9e1"
+    manifest_id_prod: UUID4 = "81513a27-f17b-4c2d-a5ae-585a2b48d73c"
     project_name: str = "xlwings Server"
     public_addin_store: bool = False
     secret_key: Optional[str] = None
